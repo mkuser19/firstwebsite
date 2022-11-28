a82059d9accb12c764824bd2b7b3cd58befe9a46
Kirti kaushal <kirtikaushal@Kirtis-MacBook-Pro.local>
2022-11-27 23:31:37 -0500

    add new code

--- src/client/welcome.php
+++ src/client/welcome.php
@@ -121,7 +121,7 @@
           <h3 class="mb-0">Featured post</h3>
           <div class="mb-1 text-muted">Nov 12</div>
           <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="stretched-link">Readmore</a>
+          <a href="readpost.php" class="stretched-link">Readmore</a>
         </div>
         <div class="col-auto d-none d-lg-block">
         <img src="../../admin images/blog1.jpg" alt= "blog1 image" width="200" height="250"> 
